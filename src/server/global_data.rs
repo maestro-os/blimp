b5d7cf58221dd6ea2ffce4088725e27db4648c9e
llenotre <llenotre@student.42.fr>
2022-06-06 00:29:55 +0200

    Added job ID generation

--- src/server/global_data.rs
+++ src/server/global_data.rs
@@ -2,6 +2,8 @@
 
 use crate::config::Config;
 use crate::job::Job;
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
 
 /// Structure storing data used all across the server.
 pub struct GlobalData {
@@ -53,9 +55,19 @@ impl GlobalData {
 			.next()
 	}
 
+	// FIXME Possible data race. Create a function that creates the job directly
 	/// Returns an unused ID for a new job.
 	pub fn new_job_id(&self) -> String {
-		// TODO
-		"TODO".to_owned()
+		loop {
+			let id: String = thread_rng()
+				.sample_iter(&Alphanumeric)
+				.take(15)
+				.map(char::from)
+				.collect();
+
+			if self.get_job(&id).is_none() {
+				return id;
+			}
+		}
 	}
 }
