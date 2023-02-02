2ff65da6c6c6d455ee812336e5a0bc19798c90c5
llenotre <llenotre@student.42.fr>
2023-02-02 17:57:01 +0100

    Removed static compilation from cross_compile_env.sh

--- cross_compile_env.sh
+++ cross_compile_env.sh
@@ -4,10 +4,9 @@
 export TARGET=i686-unknown-linux-musl
 
 export CC=clang
-#export CFLAGS="-target i686-unknown-linux-musl --sysroot $TOOLCHAIN -L$TOOLCHAIN/usr/lib -L$TOOLCHAIN/lib -fuse-ld=lld --rtlib=compiler-rt --static"
 export CFLAGS="-target i686-unknown-linux-musl --sysroot $TOOLCHAIN"
 
 export LD=ld.lld
-export LDFLAGS="-target i686-unknown-linux-musl --sysroot $TOOLCHAIN -L$TOOLCHAIN/usr/lib -L$TOOLCHAIN/lib -fuse-ld=lld --rtlib=compiler-rt --static"
+export LDFLAGS="-target i686-unknown-linux-musl --sysroot $TOOLCHAIN -L$TOOLCHAIN/usr/lib -L$TOOLCHAIN/lib -fuse-ld=lld --rtlib=compiler-rt"
 
 export RUSTFLAGS="-L$TOOLCHAIN/usr/lib -L$TOOLCHAIN/lib -Clink-arg=-fuse-ld=lld -Clinker=clang -Clink-arg=--rtlib=compiler-rt"
